be5d2cca0cd51d7e7ba155d86d38a0e8fbe7ecee
Syed Shoaib Abrar <132744941+realabrar1@users.noreply.github.com>
2024-11-15 11:35:27 +0530

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,7 +6,7 @@ const About = () => {
     {
       name: 'SYED ABRAR',
       position: 'Founder & CEO',
-      image: 'https://drive.google.com/file/d/1MfNW6QWi4CNsE6DzsD8yxg2-cDsqge-E/view?usp=drive_link',
+      image: 'https://github.com/realabrar1/images/blob/main/Pi7_Passport_Photo%20(1).jpeg?raw=true',
       bio: 'John is a passionate animal lover who founded PetsMoms to help pet owners find the best products and services for their furry friends.',
     },
     {
