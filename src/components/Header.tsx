ddb6ea0394f07c52b527916ffbc256e28cb4bf26
Syed Shoaib Abrar <132744941+realabrar1@users.noreply.github.com>
2024-11-15 10:01:37 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -96,6 +96,12 @@ export default function Header() {
             </div>
             {isAuthenticated ? (
               <>
+                 <Link
+            to="/about"
+            className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+          >
+            About
+          </Link>
                 <Link
                   to="/profile"
                   className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
