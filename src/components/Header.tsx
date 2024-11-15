4ef395c92866ffded171ec6abb55a47161b2e1fc
Syed Shoaib Abrar <132744941+realabrar1@users.noreply.github.com>
2024-11-15 10:03:06 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -123,6 +123,12 @@ export default function Header() {
               </>
             ) : (
               <>
+                 <Link
+            to="/about"
+            className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+          >
+            About
+          </Link>
                 <Link
                   to="/login"
                   className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
